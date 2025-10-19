d8be24d69e021dd9571f399247269ac97e0c18cf
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-19 10:12:50 +0530

    variables

---
+++ conditional_statements.py
@@ -0,0 +1,21 @@
+"""
+Types of Conditional Statements in Python
+Python mainly provides four forms of conditional statements:
+
+1.if statement
+2.if-else statement
+3.if-elif-else ladder
+4.Nested if statements
+
+"""
+
+"""1. if Statement -> Executes a block of code only if a given condition is True.
+if condition:
+    # code to execute when condition is true"""
+
+#Example
+
+age = 20
+if age>=20:
+    print("you are an adult because you're age is" + age)
+    
---
+++ variables.py
@@ -0,0 +1,33 @@
+name ="Gayathri"
+age=20
+gender="Female"
+
+#method 1
+print(name)
+print(age)
+print(gender)
+
+# method 2
+
+print("My name is",name)
+print("My age is",age)
+print("My gender is",gender)
+
+#method 3
+
+print("My name is",name,"my age is",age,"my gender is",gender)
+
+#method 4 (F-strings)
+
+print(f"My name is {name}" )
+print(f"My age is {age}")
+print(f"My gender is {gender}")
+
+#method 5(f-strings in single line)
+
+print(f"My name is {name} my age is {age} my gender is {gender}")
+
+
+
+
+
