f9f236c858247a172cc79563c069ac680cba3a21
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-19 10:49:03 +0530

    type conversion

---
+++ type_conversion.py
@@ -0,0 +1,31 @@
+"""
+Type conversion/ Type casting: convert one data type to another data type
+
+int ->str
+str->int
+
+"""
+
+x = "100" #string
+y = "200"
+z = x+y
+print(z)
+
+a = int(x)
+print(a, type(a))
+
+b = int(y)
+print(b,type(b))
+
+c = a+b
+print(c, type(c))
+
+x1="100.5" #str
+y1="200"
+
+a1=float(x1)
+b1=float(y1)
+c1=a1+b1
+print(c1,type(c1))
+
+
