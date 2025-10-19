46e5674cd3e0e27820a8df41938984517b3b038c
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-19 11:23:15 +0530

    user input

---
+++ add_number_input.py
@@ -0,0 +1,30 @@
+num1=input("Enter first number = ") # by default input is string
+num2=input("Enter second number = ")
+
+total = num1+num2
+print(total)
+
+x=int(num1)
+y = int(num2)
+
+z = x+y
+print(z)
+
+#other method
+
+num3=int(input("Enter first number = ")) # by default input is string
+num4=int(input("Enter second number = "))
+
+total_1=(num3+num4)
+print(total_1)
+
+# add 5 numbers and it's total
+
+n1 = int(input("Enter first number"))
+n2 = int(input("Enter second number"))
+n3 = int(input("Enter third number"))
+n4 = int(input("Enter fourth number"))
+n5 = int(input("Enter fifth number"))
+total = n1+n2+n3+n4+n5
+print(f"your total is {total}")
+
---
+++ input_function.py
@@ -0,0 +1,15 @@
+x = input("Enter your name = ")
+print(x)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+print(f"your name is {name}")
+print(f"your age is {age}")
+print(f"your gender is {gender}")
+
+
+
+
+
+
