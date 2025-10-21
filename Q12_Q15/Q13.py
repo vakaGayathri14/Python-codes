6d7c711015a1c49c2810ed0e77392d045ca7eb70
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-21 13:36:16 +0530

    swapping of 2 numbers

--- Q12_Q15/Q13.py
+++ Q12_Q15/Q13.py
@@ -8,8 +8,33 @@ a = int(input("Enter a number"))
 b = int(input("Enter another number"))
 
 print(f"{a},{b}")
+# using temp variable
 
-b = a
+temp = a
 a = b
+b = temp
+print(f"{a},{b}")
 
+
+
+# without using 3rd variable # in this methos we are wasting 3 bits so there is other approach caled xor
+
+a = a+b 
+b = a-b
+a = a-b
+
+print(f"{a},{b}")
+
+
+# XOR Approach - xor will not waste extra bits
+
+a = a^b
+b =a^b
+a =a^b
+
+print({a},{b})
+
+
+#simple approach
+a,b = b,a
 print(f"{a},{b}")
