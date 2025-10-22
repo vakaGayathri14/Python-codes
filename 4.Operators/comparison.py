c7420281e8a9291cda6f96505af153ad1a571d16
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-22 21:15:34 +0530

    comparision operators

---
+++ 4.Operators/comparison.py
@@ -0,0 +1,23 @@
+"""
+Comparison Operators: 
+
+Operator    Name                      Example
+ >          Greater than                x>y
+ <          Less than                   x<y
+ >=         Greater than or equal to    x>=y
+ <=         Less than or equal to       x<=y
+ ==         Equal                       x==y
+ !=         Not equal                   x!=y
+ 
+output of comparision operator is always be in boolean true/false
+
+"""
+
+a=20
+b=20
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a==b)
+print(a!=b)
