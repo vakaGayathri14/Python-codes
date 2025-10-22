7440ba0e0ce7ea764ebce6f333b801b79c0fe3b0
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-22 11:10:59 +0530

    Compound intrest problem

--- Q12_Q15/Q14.py
+++ Q12_Q15/Q14.py
@@ -5,3 +5,13 @@ user.
 
 """
 
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the interest rate (in %): "))
+time = float(input("Enter the time period (in years): "))
+n = int(input("Enter the number of times interest is compounded per year: "))
+
+amount = principal * (1+(rate/(100*n))) ** (n * time)
+compound = amount - principal
+
+print(f"amount: {amount}")
+print(f"compound: {compound}")
