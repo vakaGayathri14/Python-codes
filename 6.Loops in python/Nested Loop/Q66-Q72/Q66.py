9dd0734e293aa9b723cdb60be50feb489a96535a
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-30 08:58:32 +0530

    nested for loop

---
+++ 6.Loops in python/Nested Loop/Q66-Q72/Q66.py
@@ -0,0 +1,15 @@
+"""
+print the following pattern
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+"""
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print(" ")
