59834c86088f3f194242c5f78da143343887d906
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-10-30 08:33:15 +0530

    control statements

---
+++ 6.Loops in python/control_statements/break_statements.py
@@ -0,0 +1,17 @@
+# for i  in range(1,101):
+#     print(i)
+
+#now i want to go till 10 and then stop the loop
+
+# for i  in range(1,101):
+#     print(i)
+#     if i==10:
+#         break
+
+
+for i  in range(1,101):
+    if i==10:
+        break
+    print(i)
+
+    
---
+++ 6.Loops in python/control_statements/continue_statements.py
@@ -0,0 +1,7 @@
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
+    print("Done")
+    print("Bye")
+print("program finished!")
