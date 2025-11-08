a079535ba927f5ac7e203e13c201b2ded9832322
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-08 10:48:28 +0530

    empty set

---
+++ 11.Sets/empty_set.py
@@ -0,0 +1,9 @@
+my_set = {}
+print(my_set)
+print(type(my_set))  # as it is empty list it shows the type as dictionary
+
+# to overcome the above case use the below way
+
+my_set = set()
+print(my_set)
+print(type(my_set))
