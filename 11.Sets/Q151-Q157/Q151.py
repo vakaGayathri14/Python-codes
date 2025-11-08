d00234a2b8863fdb316b5740b3c07c1a9d116ff8
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-08 11:29:00 +0530

    problems on sets

---
+++ 11.Sets/Q151-Q157/Q151.py
@@ -0,0 +1,15 @@
+"""
+Q158. Given two lists a, b. Check if two lists have at least one element
+common in them.
+
+"""
+
+a = [1, 2, 3, 4]
+b = [4, 3, 5, 2]
+
+c = set(a)
+d = set(b)
+print(c)
+print(d)
+
+print(c.intersection(d))
---
+++ 11.Sets/Q151-Q157/Q152.py
@@ -0,0 +1,19 @@
+"""
+Q159. Python program to find common elements in three lists using sets.
+
+"""
+
+a = [1, 2, 3, 4]
+b = [4, 3, 5, 2]
+c = [5, 1, 2, 0, 9, 4, 5, 7]
+
+d = set(a)
+e = set(b)
+f = set(c)
+
+print(d)
+print(e)
+print(f)
+x = d.intersection(e)
+print(x)
+print(f.intersection(x))
