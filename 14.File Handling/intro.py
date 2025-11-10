69cf3a840567f4d0ed9b2fe912c0df6d15d2cf4e
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-10 22:48:39 +0530

    introduction to file handling

---
+++ 14.File Handling/intro.py
@@ -0,0 +1,12 @@
+"""
+File Handling in Python: if there ia a file it can be a ppt/text file//image it can be of anything you can read that you can write that you can append that you can rename that you can move, you can delete that you can create a new file as well.
+
+1. Read (r)
+2. Write (w)
+3. Append (a)
+4. Rename
+5. Delete
+6. Move
+7. Create a file
+
+"""
