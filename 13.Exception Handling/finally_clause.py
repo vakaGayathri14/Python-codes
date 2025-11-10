b95d5f74b2f34bc1f4b0360287118346734bc8e1
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-10 22:41:54 +0530

    finally clause

---
+++ 13.Exception Handling/finally_clause.py
@@ -0,0 +1,23 @@
+# here with try and except we can write else this is optional
+# [else clause]
+try:
+    my_list = [2, 5, 6, 7, 88, 0]
+    # print(my_list[1])
+    print(my_list[76])
+    # print(my_list[0]/my_list[-1])
+    # my_list = my_list * "abc"
+
+except IndexError:
+    print("Invalid Index")
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+except:
+    print("some error occured")
+
+# when this else part will work when we don't have any errors in the try block when the try block executes successfully then the else block will run
+else:
+    print("Everything worked fine")
+#under elseclause there will be finally clause , there is no dependent on else clause
+# finally clause will run irrespective of the error present or not even though there is error it will run
+finally:
+    print("This is a finally clause")
