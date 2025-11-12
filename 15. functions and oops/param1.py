6e163b04d3408fbd3209bf3811f017e4ed346097
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-12 15:21:53 +0530

    parameters and arguments

---
+++ 15. functions and oops/param1.py
@@ -0,0 +1,7 @@
+def addition(n1, n2):  # parameters
+    total = n1 + n2
+    print(total)
+
+
+addition(1, 2)
+addition(10, 2)  # arguments
---
+++ 15. functions and oops/param2.py
@@ -0,0 +1,9 @@
+# write a function that is a list and which do addition of it
+def additionOfList(num1):  # parameters
+    total = 0
+    for i in num1:
+        total = total + i
+    print(total)
+
+
+additionOfList([1, 20, 3])  # Arguments
