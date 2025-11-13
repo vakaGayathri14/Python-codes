508ff30ef6a7bd916c5f5129a6070b8fb1cfe999
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-13 12:30:14 +0530

    annotations

--- 15. functions and oops/anotations.py
+++ 15. functions and oops/anotations.py
@@ -5,3 +5,32 @@ def add(x: int, y: int):
 
 add(10, 20)
 add("10", "20")
+
+
+def add(x: int, y: int):
+    total = x + y
+    return total
+
+
+c = add(10, 20)
+print(c)
+
+
+# other way if you write -> int: then the output is also int just for display purpose
+def add(x: int, y: int) -> int:  # [return is also integer]
+    total = x + y
+    return total
+
+
+c = add(10, 20)
+print(c)
+
+
+def greet(name: str, age: int, percentage: float) -> None:
+    print(name)
+    print(age)
+    print(percentage)
+
+
+greet()
+
---
+++ 15. functions and oops/list1.py
@@ -0,0 +1,7 @@
+a = [18, 82, 93, 4, 5]
+
+a.sort()
+print(a)
+
+c = a.count(82)
+print(c)
