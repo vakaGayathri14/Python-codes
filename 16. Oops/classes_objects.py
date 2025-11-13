e8baf9c5cfef3476dbddf5f5f2c129a888d490d1
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-13 21:32:09 +0530

    init

--- 16. Oops/classes_objects.py
+++ 16. Oops/classes_objects.py
@@ -59,3 +59,52 @@ s1.info()
 s2.set_info()
 print("-----")
 s2.info()
+
+#now if i comment the attributes in the class then also it will work without an error 
+    # roll_no = 0
+    # name = ""
+    # age = 0
+    # gender = ""
+    # address = ""
+
+# because in self_info function if i don't have any attributes inside a class it will create a new attribute using this
+        # self.name=input("Enter name = ") # here name variable will create inside a class
+        # self.age=int(input("Enter age = ")) # here age variable will create inside a class
+        # self.gender=input("Enter gender = ") # here gender variable will create inside a class
+        # self.roll_no=int(input("Enter roll number = ")) # here roll no variable will create inside a class
+
+# if i do s1.info() it will print the values inside the class of that object s1
+
+# now if i call s1.info() prior than set_info() creating attributes then I will get an error because i don't have attributes itsef how can i print them
+
+#now if we 2 objects then we can first set the attributes and then print the info of them for example if i have  100 objects it might be difficult to over come to check that each steps are going correctly for 100 right then we have a concept of special method called "init"
+
+#syntax
+
+class Subject1:
+    # init is an initializer 
+    def __init__(self): # this is how the syntax is you should not change
+        # print("In INIT") # this block inside the init method will run automatically if we create a new object
+        self.name=input("Enter name = ")
+        self.age=int(input("Enter age = "))
+        self.gender=input("Enter gender = ")
+        self.roll_no=int(input("Enter roll number = "))
+
+    def info(self):
+        print(f"Name = {self.name}") # whose name/age/gender -> which objects calls this those name/age/gender will be printed
+        print(f"Age = {self.age}")
+        print(f"Gender = {self.gender}")
+        print(f"Roll no = {self.roll_no}")
+
+
+s1 = Subject1()
+s1.info()
+
+s2 = Subject1()
+s2.info()
+
+
+
+
+
+
