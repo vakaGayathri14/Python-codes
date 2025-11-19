4679a4e02668e5637df7880038d144d51dee8aaa
vakaGayathri14 <gayathrireddy55110@gmail.com>
2025-11-19 15:59:42 +0530

    acess modifiers (public, private)

--- 16. Oops/Encapsulation/encapsulation.py
+++ 16. Oops/Encapsulation/encapsulation.py
@@ -1,84 +1,85 @@
-# # Encapsulation: Encapsulation is ntg but Data hiding , data hiding through access modifiers
-# from random import randint
+# Encapsulation: Encapsulation is ntg but Data hiding , data hiding through access modifiers
+from random import randint
 
 
-# class Bank:
-#     def __init__(self) -> None:
-#         self.name = input("Enter name = ")
-#         self.account_no = randint(100000, 999999)
-#         self.balance = 0
+class Bank:
+    def __init__(self) -> None:
+        self.name = input("Enter name = ")
+        self.account_no = randint(100000, 999999)
+        self.balance = 0
 
-#     def display(self) -> None:
-#         print(f"Name = {self.name}")
-#         print(f"Account no = {self.account_no}")
-#         print(f"Balance = {self.balance}")
+    def display(self) -> None:
+        print(f"Name = {self.name}")
+        print(f"Account no = {self.account_no}")
+        print(f"Balance = {self.balance}")
 
-# obj = Bank()
-# obj.display() # here account no is randomly generated one
-# print("--------------")
+obj = Bank()
+obj.display() # here account no is randomly generated one
+print("--------------")
 
-# obj.account_no =1 # here when i assign 1 to account no then account no will be now 1 not the random value but this is wrongit should not change like this then we have a concept of access modifiers
-# obj.display()
+obj.account_no =1 # here when i assign 1 to account no then account no will be now 1 not the random value but this is wrongit should not change like this then we have a concept of access modifiers
+obj.display()
 
-# access modifiers -> will say that that variable should access only within the class not outside of the class
-# for this there are 3 they are below 
-# public, protected, private, 
+""" access modifiers -> will say that that variable should access only within the class not outside of the class
+for this there are 3 they are below 
+public, protected, private, 
 
-# public :- class variables can even access outside the class those are called public here above is the way of public 
-# private :- only accessible inside the class cannot be acessible outside the class, for this for variable  have to write __ prior to the name
-# example below
+public :- class variables can even access outside the class those are called public here above is the way of public 
+private :- only accessible inside the class cannot be acessible outside the class, for this for variable  have to write __ prior to the name
+example below """
 
 print("----------------------")
 
 # Encapsulation: Encapsulation is ntg but Data hiding , data hiding through access modifiers
-# from random import randint
+from random import randint
 
 
-# class Bank:
-#     def __init__(self) -> None:
-#         self.name = input("Enter name = ")
-#         self.__account_no = randint(100000, 999999)
-#         self.__balance = 0
+class Bank:
+    def __init__(self) -> None:
+        self.name = input("Enter name = ")
+        self.__account_no = randint(100000, 999999)
+        self.__balance = 0
 
-#     def display(self) -> None:
-#         print(f"Name = {self.name}")
-#         print(f"Account no = {self.__account_no}")
-#         print(f"Balance = {self.__balance}")
+    def display(self) -> None:
+        print(f"Name = {self.name}")
+        print(f"Account no = {self.__account_no}")
+        print(f"Balance = {self.__balance}")
 
-# obj = Bank()
-# obj.display() # here account no is randomly generated one
-# print("--------------")
+obj = Bank()
+obj.display() # here account no is randomly generated one
+print("--------------")
 
-# obj.account_no =1 # here when i assign 1 to account no then account no will not change because we are using private
-# obj.__account_no =1# same as above no change
-# obj.display()
+obj.account_no =1 # here when i assign 1 to account no then account no will not change because we are using private
+obj.__account_no =1# same as above no change
+obj.display()
 
 print("----------------------")
 
 # now i want to display only balance of that object we basically say that as getter and setter
 
-# from random import randint
+from random import randint
 
 
-# class Bank:
-#     def __init__(self) -> None:
-#         self.name = input("Enter name = ")
-#         self.__account_no = randint(100000, 999999)
-#         self.__balance = 0
+class Bank:
+    def __init__(self) -> None:
+        self.name = input("Enter name = ")
+        self.__account_no = randint(100000, 999999)
+        self.__balance = 0
 
-#     def display_balance(self): # getter
-#         print(self.__balance)
+    def display_balance(self): # getter
+        print(self.__balance)
 
-#     def display(self) -> None:
-#         print(f"Name = {self.name}")
-#         print(f"Account no = {self.__account_no}")
-#         print(f"Balance = {self.__balance}")
+    def display(self) -> None:
+        print(f"Name = {self.name}")
+        print(f"Account no = {self.__account_no}")
+        print(f"Balance = {self.__balance}")
 
-# obj = Bank()
-# obj.display() # here account no is randomly generated one
-# print("--------------")
+obj = Bank()
+obj.display() # here account no is randomly generated one
+print("--------------")
 
-# print(obj.display_balance()) # getter here that display balance function is public so i can access outside the class
+
+print(obj.display_balance()) # getter here that display balance function is public so i can access outside the class
 
 
 print("----------------------")
